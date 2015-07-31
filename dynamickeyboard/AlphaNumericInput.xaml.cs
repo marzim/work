cedce5533204013fb57d4979ff2ec89ddc0366b5
unknown <mc185104@WPHMC185104-WMR.corp.ncr.com>
2015-07-31 20:47:52 +0800

    commit dynamic keyboard

---
+++ dynamickeyboard/AlphaNumericInput.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using SSCOUIModels.Controls;
+using FPsxWPF.Controls;
+using SSCOUIModels;
+using SSCOControls;
+using System.ComponentModel;
+using System.Security;
+using System.Globalization;
+
+namespace SSCOUIViews.Views
+{
+    /// <summary>
+    /// Interaction logic for Search.xaml
+    /// </summary>
+    public partial class AlphaNumericInput : PopupView
+    {
+        public AlphaNumericInput(IMainViewModel viewModel)
+            : base(viewModel)
+        {
+            InitializeComponent();
+            this.GenericAlphaNumericKeyboard.initKeyboardProperties();
+            removeAddEvents();
+        }
+         
+        /// <summary>
+        /// Keyboard click events
+        /// </summary>
+        /// <param name="sender">This is a parameter with a type of object</param>
+        /// <param name="e">This is a parameter with a type of RoutedEventArgs</param>
+        private void KeyboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            WatermarkTextBox textBox = Keyboard.FocusedElement as WatermarkTextBox;
+            if (null == textBox)
+            {
+                return;
+            }
+            ImageButton button = sender as ImageButton;
+            if (null != button.CommandParameter)
+            {
+                Key key = Key.None;
+                try
+                {
+                    key = (Key)new KeyConverter().ConvertFromString(button.CommandParameter.ToString());
+                }
+                catch (InvalidCastException)
+                {
+                }
+                if (key != Key.None && key != Key.Space)
+                {
+                    textBox.RaiseEvent(new KeyEventArgs(Keyboard.PrimaryDevice, PresentationSource.FromVisual(textBox), 0, key) { RoutedEvent = Keyboard.KeyDownEvent });
+                }
+                else if (key == Key.Space)
+                {
+                    textBox.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice,
+                        new TextComposition(InputManager.Current, textBox, " ")) { RoutedEvent = TextCompositionManager.TextInputEvent });
+                }
+            }
+            else
+            {
+                textBox.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice,
+                        new TextComposition(InputManager.Current, textBox, button.Text)) { RoutedEvent = TextCompositionManager.TextInputEvent });
+            }
+        }
+
+        /// <summary>
+        /// Preview KeyDown events to have our own handling for Space key
+        /// </summary>
+        /// <param name="sender">This is a parameter with a type of object</param>
+        /// <param name="e">This is a parameter with a type of KeyEventArgs</param>
+        private void InputTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            WatermarkTextBox textBox = Keyboard.FocusedElement as WatermarkTextBox;
+            if (null == textBox)
+            {
+                return;
+            }
+            if (e.Key == Key.Space)
+            {
+                textBox.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice,
+                        new TextComposition(InputManager.Current, textBox, " ")) { RoutedEvent = TextCompositionManager.TextInputEvent });
+                e.Handled = true;
+            }
+        }
+
+        
+        public override void Show(bool isShowing)
+        {
+            if (isShowing)
+            {
+                this.GenericAlphaNumericKeyboard.retrieveCustomKeyboardProperties();
+                SetFocus(GenericAlphaNumericKeyboard.InputTextBox);
+                this.GenericAlphaNumericKeyboard.ShiftButton.IsChecked = false;
+                this.GenericAlphaNumericKeyboard.ToggleShift(false);
+                this.GenericAlphaNumericKeyboard.InputTextBox.Clear();
+                this.GenericAlphaNumericKeyboard.InputTextBox.PasswordMode = false;
+            }
+            else
+            {
+                this.GenericAlphaNumericKeyboard.removeEvents();
+                this.GenericAlphaNumericKeyboard.InputTextBox.PreviewKeyDown -= InputTextBox_PreviewKeyDown;
+                this.GenericAlphaNumericKeyboard.EnterOperatorButton.Click -= EnterButton_Click;
+                this.GenericAlphaNumericKeyboard.KeyboardButtonClick -= KeyboardButton_Click;                
+            }
+            base.Show(isShowing);
+        }
+
+        /// <summary>
+        /// OnStateParamChanged that accepts param that is set in app.config
+        /// </summary>
+        /// <param name="param">String type of param.</param>
+        public override void OnStateParamChanged(string param)
+        {
+            this.Instructions.Visibility = contextForInstructionControlShow.Contains(param) ? Visibility.Visible : Visibility.Collapsed;
+            UpdateText("Leadthrutext", this.TitleControl);
+            UpdateText("MessageBoxNoEcho", this.Message);
+            UpdateText("InstructionScreenTitle", this.Instructions);
+            
+            if (param.Equals("OperatorKeyboard"))
+            {
+                this.Instructions.Text = SSCOUIStrings.Properties.Resources.EnterIDText;
+                this.Message.Text = SSCOUIStrings.Properties.Resources.EnterIDMessage;                    
+            }
+            this.TitleControl.Visibility = contextForTitleControlShow.Contains(param) ? 
+                System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// EnterButton click events
+        /// </summary>
+        /// <param name="sender">This is a parameter with a type of object</param>
+        /// <param name="e">This is a parameter with a type of RoutedEventArgs</param>
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string commandParam = String.Empty;
+            if (null != viewModel.StateParam)
+            {
+                switch (viewModel.StateParam)
+                {
+                    case "Keyboard0409":
+                    case "OperatorKeyboard":
+                        commandParam = "EnterKeyboard({0})";
+                        break;
+                    case "ExtendedAlphaNumeric":
+                        commandParam = "EnterAlphaNum({0}?@?AtSignKey?.?KeyBoardP3?-?MinusKey)";
+                        break;
+                    default:
+                        commandParam = "EnterAlphaNum({0})";
+                        break;
+                }
+            }
+            this.GenericAlphaNumericKeyboard.EnterOperatorButton.CommandParameter = String.Format(commandParam, this.GenericAlphaNumericKeyboard.InputTextBox.PasswordMode ? this.GenericAlphaNumericKeyboard.InputTextBox.PasswordText : this.GenericAlphaNumericKeyboard.InputTextBox.Text);
+            this.GenericAlphaNumericKeyboard.InputTextBox.Text = string.Empty;
+        }
+
+        public override void OnPropertyChanged(string name, object value)
+        {
+            if (name.Equals("NextGenData") && viewModel.StateParam.Equals("OperatorKeyboard"))
+            {
+                SetFocus(GenericAlphaNumericKeyboard.InputTextBox);
+                GenericAlphaNumericKeyboard.InputTextBox.Clear();                
+                if (value.ToString().Equals("EnterPassword"))
+                {
+                    this.Instructions.Text = SSCOUIStrings.Properties.Resources.EnterPasswordText;
+                    Message.Text = SSCOUIStrings.Properties.Resources.EnterPasswordMessage;
+                    GenericAlphaNumericKeyboard.InputTextBox.PasswordMode = true;
+                }
+                else if (value.ToString().Equals("EnterID"))
+                {
+                    this.Instructions.Text = SSCOUIStrings.Properties.Resources.EnterIDText;
+                    Message.Text = SSCOUIStrings.Properties.Resources.EnterIDMessage;
+                    GenericAlphaNumericKeyboard.InputTextBox.PasswordMode = false;
+                }
+            }
+            else if (name.Equals("Leadthrutext"))
+            {
+                UpdateText(name, this.TitleControl);
+            }
+            else if (name.Equals("MessageBoxNoEcho"))
+            {
+                UpdateText(name, this.Message);
+            }
+            else if (name.Equals("InstructionScreenTitle"))
+            {
+                UpdateText(name, this.Instructions);
+            }
+            else if (name.Equals("UIEchoField"))
+            {
+                MinInput = viewModel.UIEchoField.MinLength;
+                GenericAlphaNumericKeyboard.InputTextBox.MaxLength = viewModel.UIEchoField.MaxLength;
+            }
+        }
+
+        private void UpdateText(string property, TextBlock tb)
+        {
+            string propertyText = GetPropertyStringValue(property);
+
+            if (propertyText.Length > 0)
+            {
+                string[] headerText = propertyText.Split(':');
+                tb.Text = headerText[0];
+                if (headerText.Length > 1)
+                {
+                    tb.Text = tb.Text + ":\n" + headerText[1];
+                }
+            }
+        }
+        
+        private void removeAddEvents()
+        {        
+            this.GenericAlphaNumericKeyboard.KeyboardButtonClick -= KeyboardButton_Click;
+            this.GenericAlphaNumericKeyboard.KeyboardButtonClick += KeyboardButton_Click;
+            this.GenericAlphaNumericKeyboard.EnterButtonClick -= EnterButton_Click;
+            this.GenericAlphaNumericKeyboard.EnterButtonClick += EnterButton_Click;
+            this.GenericAlphaNumericKeyboard.InputTextBox.PreviewKeyDown -= InputTextBox_PreviewKeyDown;            
+            this.GenericAlphaNumericKeyboard.InputTextBox.PreviewKeyDown += InputTextBox_PreviewKeyDown;            
+        }
+
+        private int MinInput = 1;
+
+        private static List<string> contextForTitleControlShow = new List<string>() { "AlphaNumeric", "ExtendedAlphaNumeric", 
+            "AlphaNumericPassword" };
+
+        private static List<string> contextForInstructionControlShow = new List<string>() { "Keyboard0409", "OperatorKeyboard", 
+            "AlphaNumericPassword", "Keyboard041f", "Keyboard0c0c", "Keyboard080a", "Keyboard0424", "Keyboard040c" };
+    } 
+}
---
+++ dynamickeyboard/controls/AlphaNumericKeyboard.xaml.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SSCOUIModels;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using FPsxWPF.Controls;
+using SSCOControls;
+using System.Windows.Controls.Primitives;
+using SSCOUIModels.Helpers;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Collections.Specialized;
+
+namespace SSCOUIViews.Controls
+{
+    /// <summary>
+    /// Interaction logic for AlphaNumericKeyboard.xaml
+    /// </summary>
+    public partial class AlphaNumericKeyboard : Grid
+    {
+        public AlphaNumericKeyboard()            
+        {            
+            InitializeComponent();
+        }
+
+        public void initKeyboardProperties()
+        {   
+            Line1KeysCollection = viewModel.GetPropertyValue("Line2AlphaNumericKeys") as ObservableCollection<GridItem>;
+            Line2KeysCollection = viewModel.GetPropertyValue("Line3AlphaNumericKeys") as ObservableCollection<GridItem>;
+            Line3KeysCollection = viewModel.GetPropertyValue("Line4AlphaNumericKeys") as ObservableCollection<GridItem>;
+            Line4KeysCollection = viewModel.GetPropertyValue("Line1AlphaNumericKeys") as ObservableCollection<GridItem>;
+            removeAddEvents();
+        }
+        
+        public void retrieveCustomKeyboardProperties()
+        {
+            Line1AlphaNumKeys = UIChildFinder.FindVisualChild<UniformGrid>(Line1Keys, "Line1AlphaNumKeys") as UniformGrid;
+            Line2AlphaNumKeys = UIChildFinder.FindVisualChild<UniformGrid>(Line2Keys, "Line2AlphaNumKeys") as UniformGrid;
+            Line3AlphaNumKeys = UIChildFinder.FindVisualChild<UniformGrid>(Line3Keys, "Line3AlphaNumKeys") as UniformGrid;
+            bindAllLineKeys();
+        }        
+
+        private void Grid_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            this.viewModel = DataContext as IMainViewModel;            
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            RoutedEventHandler tempHandler = EnterButtonClick;
+            if (null != tempHandler)
+            {
+                tempHandler(sender, e);
+            }
+        }
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RoutedEventHandler tempHandler = InputTextBoxTextChanged;
+            if (null != tempHandler)
+            {
+                tempHandler(sender, e);
+            }
+            else
+            {
+                handleBaseInputTextBox_TextChange();
+            }            
+        }
+
+        private void handleBaseInputTextBox_TextChange()
+        {
+            this.BSButton.IsEnabled = (this.InputTextBox.Text.Length > 0);
+            this.EnterOperatorButton.IsEnabled = (this.InputTextBox.Text.Length >= MinInput);
+        }
+
+        private void KeyboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            RoutedEventHandler tempHandler = KeyboardButtonClick;
+            if (null != tempHandler)
+            {
+                tempHandler(sender, e);
+            }
+            else
+            {
+                handleBaseKeyboardButton_Click(sender);
+            }
+        }
+
+        private void handleBaseKeyboardButton_Click(object sender)
+        {
+            WatermarkTextBox textBox = Keyboard.FocusedElement as WatermarkTextBox;
+            if (null == textBox)
+            {
+                return;
+            }
+            ImageButton button = sender as ImageButton;
+            if (null != button.CommandParameter)
+            {
+                Key key = Key.None;
+                try
+                {
+                    key = (Key)new KeyConverter().ConvertFromString(button.CommandParameter.ToString());
+                }
+                catch (InvalidCastException)
+                {
+                }
+                if (key != Key.None)
+                {
+                    textBox.RaiseEvent(new KeyEventArgs(Keyboard.PrimaryDevice, PresentationSource.FromVisual(textBox), 0, key) { RoutedEvent = Keyboard.KeyDownEvent });
+                }                
+            }
+            else
+            {
+                textBox.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice,
+                    new TextComposition(InputManager.Current, textBox, button.Text)) { RoutedEvent = TextCompositionManager.TextInputEvent });
+            }
+        }
+
+        private void ShiftButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ToggleShift(ShiftButton.IsChecked.Value);
+        }
+
+        public void ToggleShift(bool isUpper)
+        {
+            object[] rows = { Line1AlphaNumKeys, Line2AlphaNumKeys, Line3AlphaNumKeys };
+            
+            for (int i = 0; i < rows.Length; i++)
+            {
+                foreach (ImageButton button in UIChildFinder.FindVisualChildren<ImageButton>(rows[i] as System.Windows.Controls.Primitives.UniformGrid))
+                {
+                    button.Text = isUpper ? button.Text.ToUpper() : button.Text.ToLower();
+                }
+            }
+        }
+
+        private void InputTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            RoutedEventHandler tempHandler = InputTextBoxPreviewKeydown;
+            if (null != tempHandler)
+            {
+                tempHandler(sender, e);
+            }
+            else
+            {
+                handleBaseInputTextBox_PreviewKeydown(sender, e);
+            }
+        }
+
+        private void handleBaseInputTextBox_PreviewKeydown(object sender, KeyEventArgs e)
+        {
+            WatermarkTextBox textBox = Keyboard.FocusedElement as WatermarkTextBox;
+            if (null == textBox)
+            {
+                return;
+            }
+            if (e.Key == Key.Enter && InputTextBox.Text.Length > 0)
+            {
+                invokeButtonClick(EnterOperatorButton);
+                e.Handled = true;
+                return;
+            }
+            else if (e.Key == Key.Space)
+            {
+                textBox.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice,
+                        new TextComposition(InputManager.Current, textBox, " ")) { RoutedEvent = TextCompositionManager.TextInputEvent });
+                e.Handled = true;
+                return;
+            }            
+        }
+
+        private void invokeButtonClick(Button btn)
+        {            
+            ButtonAutomationPeer peer = new ButtonAutomationPeer(btn);
+            IInvokeProvider invokeProvider = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+            if (null != invokeProvider)
+            {
+                invokeProvider.Invoke();
+            }            
+        }
+
+        private void Line1KeysCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {   
+            if (e.Action == NotifyCollectionChangedAction.Reset || (e.Action == NotifyCollectionChangedAction.Replace && replaceBindValues(sender)))
+            //if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                
+                bindLineKeys(Line1KeysCollection, Line1Keys, Line1AlphaNumKeys, "AlphaNumP1", "Line1Alpha");
+            }
+        }
+        
+        private void Line2KeysCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                bindLineKeys(Line2KeysCollection, Line2Keys, Line2AlphaNumKeys, "AlphaNumP4", "");                
+            }
+        }
+        
+        private void Line3KeysCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                bindLineKeys(Line3KeysCollection, Line3Keys, Line3AlphaNumKeys, "AlphaNumP2", "");                
+            }
+        }
+
+        private void Line4KeysCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                bindLineKeys(Line4KeysCollection, NumericLine1Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=1");
+                bindLineKeys(Line4KeysCollection, NumericLine2Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=2");
+                bindLineKeys(Line4KeysCollection, NumericLine3Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=3");
+                bindLineKeys(Line4KeysCollection, NumericLine4Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=4");
+            }
+        }
+
+        private bool replaceBindValues(object sender)
+        {
+            var gridItem = sender as ObservableCollection<GridItem>;
+            if (null != gridItem && gridItem[gridItem.Count - 1].Text != null && gridItem[gridItem.Count - 1].Data != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void bindAllLineKeys()
+        {
+            bindLineKeys(Line1KeysCollection, Line1Keys, Line1AlphaNumKeys, "AlphaNumP1", "Line1Alpha");
+            bindLineKeys(Line2KeysCollection, Line2Keys, Line2AlphaNumKeys, "AlphaNumP4", "");
+            bindLineKeys(Line3KeysCollection, Line3Keys, Line3AlphaNumKeys, "AlphaNumP2", "");
+            bindLineKeys(Line4KeysCollection, NumericLine1Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=1");
+            bindLineKeys(Line4KeysCollection, NumericLine2Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=2");
+            bindLineKeys(Line4KeysCollection, NumericLine3Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=3");
+            bindLineKeys(Line4KeysCollection, NumericLine4Keys, null, "AlphaNumP1", "Line1Numeric;Columns=3;Row=4");
+        }
+
+        private void bindLineKeys(ObservableCollection<GridItem> items, ItemsControl lineKey, UniformGrid alphaNumKeys, string propertyName, string converterParam)
+        {
+            if (null != items)
+            {
+                MultiBinding mb = new MultiBinding();
+                mb.ConverterParameter = converterParam;
+                mb.Converter = new AlphaNumericKeysHandler();
+                mb.Bindings.Add(new Binding() { Source = items });
+                mb.Bindings.Add(new Binding() { Source = viewModel.GetPropertyValue(propertyName) });
+                BindingOperations.SetBinding(lineKey, ItemsControl.ItemsSourceProperty, mb);
+                if (null != alphaNumKeys)
+                {
+                    alphaNumKeys.Columns = (lineKey.ItemsSource as ObservableCollection<GridItem>).Count;
+                }            
+            }            
+        }
+
+        private void HotKeysKeyboardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeAddEvents()
+        {
+            Line1KeysCollection.CollectionChanged -= Line1KeysCollection_CollectionChanged;
+            Line1KeysCollection.CollectionChanged += Line1KeysCollection_CollectionChanged;
+            Line2KeysCollection.CollectionChanged -= Line2KeysCollection_CollectionChanged;
+            Line2KeysCollection.CollectionChanged += Line2KeysCollection_CollectionChanged;
+            Line3KeysCollection.CollectionChanged -= Line3KeysCollection_CollectionChanged;
+            Line3KeysCollection.CollectionChanged += Line3KeysCollection_CollectionChanged;
+            Line4KeysCollection.CollectionChanged -= Line4KeysCollection_CollectionChanged;
+            Line4KeysCollection.CollectionChanged += Line4KeysCollection_CollectionChanged;
+        }
+
+        public void removeEvents()
+        {
+            Line1KeysCollection.CollectionChanged -= Line1KeysCollection_CollectionChanged;
+            Line2KeysCollection.CollectionChanged -= Line2KeysCollection_CollectionChanged;
+            Line3KeysCollection.CollectionChanged -= Line3KeysCollection_CollectionChanged;
+            Line4KeysCollection.CollectionChanged -= Line4KeysCollection_CollectionChanged;            
+        }
+        
+        private int MinInput = 1;
+        IMainViewModel viewModel;
+
+        public event RoutedEventHandler KeyboardButtonClick;
+        public event RoutedEventHandler EnterButtonClick;
+        public event RoutedEventHandler InputTextBoxTextChanged;
+        public event RoutedEventHandler InputTextBoxPreviewKeydown;
+        private UniformGrid Line1AlphaNumKeys;
+        private UniformGrid Line2AlphaNumKeys;
+        private UniformGrid Line3AlphaNumKeys;
+        private ObservableCollection<GridItem> Line1KeysCollection;
+        private ObservableCollection<GridItem> Line2KeysCollection;
+        private ObservableCollection<GridItem> Line3KeysCollection;
+        private ObservableCollection<GridItem> Line4KeysCollection;
+
+        
+    }
+}
